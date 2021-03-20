3236867c9d3338509a12cdc371324f7e322da342
David Baldauf <david.baldauf@rbag-musik.de>
2021-03-20 19:46:59 +0100

    Dateien wie bei Fiber Recepies

---
+++ main.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"log"
+
+	"github.com/D-Bald/fiber-backend/database"
+	"github.com/D-Bald/fiber-backend/router"
+
+	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/cors"
+	_ "github.com/jinzhu/gorm/dialects/postgres"
+)
+
+func main() {
+	app := fiber.New()
+	app.Use(cors.New())
+
+	database.ConnectDB()
+
+	router.SetupRoutes(app)
+	log.Fatal(app.Listen(":3000"))
+
+	defer database.DB.Close()
+}
