325561807519c938e9e308ca4c560f46b12e375b
David Baldauf <david.baldauf@rbag-musik.de>
2021-05-05 18:03:01 +0200

    Fix for dockerfile and docker-compose.yaml

--- database/connect.go
+++ database/connect.go
@@ -14,7 +14,7 @@ import (
 // Database settings (insert your own database name and connection URI)
 var dbName = config.Config("DB_NAME")
 var mongoAtlasURI = fmt.Sprintf("mongodb+srv://%s:%s@fiber-backend.kooym.mongodb.net/%s?retryWrites=true&w=majority", config.Config("DB_USER"), config.Config("DB_USER_PASSWORD"), dbName)
-var mongoDockerURI = fmt.Sprintf("mongodb://%s:%s@mongo:%s", config.Config("DB_USER"), config.Config("DB_USER_PASSWORD"), config.Config("MONGO_PORT"))
+var mongoDockerURI = fmt.Sprintf("mongodb://%s:%s@localhost:%s", config.Config("DB_USER"), config.Config("DB_USER_PASSWORD"), config.Config("MONGO_PORT"))
 
 func Connect() error {
 	var client *mongo.Client
